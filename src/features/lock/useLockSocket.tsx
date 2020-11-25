eff8180fbc7938c881184c8e14d92f8c6fe96470
Armando Magalhães <amagalhaes@netflix.com>
2020-11-25 23:44:17 +0100

    feat: add useLockSocket

---
+++ src/features/lock/useLockSocket.tsx
@@ -0,0 +1,25 @@
+import { useLocalStorage } from 'react-use';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+
+interface LockState {
+  isLocked: boolean;
+}
+
+const defaultState: LockState = {
+  isLocked: false,
+};
+
+export const useLockSocket = (): LockState => {
+  const socketUrl = 'ws://localhost:4000/ws';
+  const { lastMessage, readyState } = useWebSocket(socketUrl, {
+    onOpen: () => console.log('WebSocket connection established.'),
+    /** Will attempt to reconnect on all close events, such as server shutting down */
+    shouldReconnect: (_closeEvent) => true,
+  });
+
+  if (readyState !== ReadyState.OPEN || !lastMessage) {
+    return defaultState;
+  }
+
+  return JSON.parse(lastMessage.data) as LockState;
+};
