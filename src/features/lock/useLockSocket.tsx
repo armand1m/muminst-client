6a6d10e148a7914964a5913a019d812d2718a9ab
Armando Magalhães <amagalhaes@netflix.com>
2020-11-26 01:40:03 +0100

    fix: enhance socket logs

--- src/features/lock/useLockSocket.tsx
+++ src/features/lock/useLockSocket.tsx
@@ -4,20 +4,29 @@ interface LockState {
   isLocked: boolean;
 }
 
-const defaultState: LockState = {
-  isLocked: false,
-};
-
 export const useLockSocket = (): LockState => {
-  const socketUrl = 'ws://localhost:4000/ws';
+  const socketUrl =
+    String(process.env.REACT_APP_API_URL).replace('http', 'ws') +
+    'ws';
   const { lastMessage, readyState } = useWebSocket(socketUrl, {
     onOpen: () => console.log('WebSocket connection established.'),
     /** Will attempt to reconnect on all close events, such as server shutting down */
     shouldReconnect: (_closeEvent) => true,
+    onError: console.error,
+    onMessage: console.info,
+    onClose: console.log,
   });
 
-  if (readyState !== ReadyState.OPEN || !lastMessage) {
-    return defaultState;
+  if (readyState !== ReadyState.OPEN) {
+    return {
+      isLocked: true,
+    };
+  }
+
+  if (!lastMessage) {
+    return {
+      isLocked: false,
+    };
   }
 
   return JSON.parse(lastMessage.data) as LockState;
